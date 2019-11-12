b628b180b5447782a23cc1899c45ddc449f5473a
GamesDevDave <gamesdevdave@gmail.com>
2019-11-12 11:06:25 +0000

    Added SceneManager.cs, Also implemented an InputField to ask for a frequency to be used in audio manipulation.

---
+++ Contract #4 - User Interface Audio (Non-Diegetic Audio)/Assets/Scripts/PlayTone.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayTone : MonoBehaviour
+{
+    public AudioClip musicTrack;
+    private AudioSource source;
+    private bool musicPlaying;
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+        musicTrack = Resources.Load<AudioClip>("music");
+        musicPlaying = false;
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            if (!musicPlaying)
+            {
+                source.Play();
+                musicPlaying = true;
+            }
+            else
+            {
+                source.Stop();
+                musicPlaying = false;
+            }
+        }
+    }
+}
