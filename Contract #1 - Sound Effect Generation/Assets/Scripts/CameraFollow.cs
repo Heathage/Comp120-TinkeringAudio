203d74b0b302c7ac73da7f9a913dff0394e4fcbe
Heathage <jh230155@falmouth.ac.uk>
2019-11-22 22:43:54 +0000

    Edited Move Script so that the ball can move Edited CameraFollow Script so that the camera will follow ball

--- Contract #1 - Sound Effect Generation/Assets/Scripts/CameraFollow.cs
+++ Contract #1 - Sound Effect Generation/Assets/Scripts/CameraFollow.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    float min = 0;
+    float max = 0;
+    public Transform target;
+    private Vector3 offset;
+
     void Start()
     {
-        
+        offset = this.transform.position - target.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        this.transform.position = offset + target.position;
+
+        //this.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
     }
 }
--- Contract #1 - Sound Effect Generation/Assets/Scripts/Move.cs
+++ Contract #1 - Sound Effect Generation/Assets/Scripts/Move.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
+    private Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
+
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(move);
     }
 }
