f02cd2e2340a119100d2ff3eec3eb0c5311d87f5
Heathage <jh230155@falmouth.ac.uk>
2019-11-27 23:35:34 +0000

    Created more appropriate comments on code.

--- Contract #1 - Sound Effect Generation/Assets/Scripts/AudioTinker.cs
+++ Contract #1 - Sound Effect Generation/Assets/Scripts/AudioTinker.cs
@@ -11,22 +11,35 @@ public class AudioTinker : MonoBehaviour {
     private AudioSource audioSource;
     private AudioClip outAudioClip;
 
+    //This is so that I can create a random frequency to be passed so that the same tone is not always played.
     int ranFreq;
+    //This allows me to create a value that I can use to increase the frequency of a tone when a particular action is completed. 
     int totalPickups = 0;
 
+    /// <summary>
+    /// Accesses the audio source component on the Game Object in unity. Could not play tones without it. 
+    /// </summary>
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
     }
     
-    //Used to play the tone generated
-    
-    public void PlayOutAudio() {
-        audioSource.PlayOneShot(outAudioClip);    
+    /// <summary>
+    /// Plays the tone that is generated, method allows me to easily call the function.
+    /// </summary>
+    public void PlayOutAudio() 
+    {
+        audioSource.PlayOneShot(outAudioClip);
     }
         
-    // Generates the tone with reference to the frequency passed
-    
+    /// <summary>
+    /// Takes the parameters passed through from other functions and uses them to create tones. They vary based on the values passed.
+    /// This is needed to create the tones that the player hears. 
+    /// </summary>
+    /// <param name="frequency"></param>
+    /// <param name="duration"></param>
+    /// <param name="totalPickups"></param>
+    /// <returns></returns>
     private AudioClip CreateToneAudioClip(int frequency, float duration, int totalPickups) 
     {
         float sampleDurationSecs = duration;
@@ -48,10 +61,12 @@ public class AudioTinker : MonoBehaviour {
         return audioClip;
     }
 
-    //Generates a tone for picking up objects and plays it.
-
+    /// <summary>
+    /// When a player picks up a pink cube, plays a tone with these values to signify successful collection.
+    /// </summary>
     public void PickUpSound()
     {
+        //The frequency of the tone gradually increases as more pink cubes are picked up. 
         totalPickups = totalPickups + 25;
         Debug.Log("Picked up");
         Debug.Log(ranFreq);
@@ -59,8 +74,9 @@ public class AudioTinker : MonoBehaviour {
         PlayOutAudio();
     }
 
-    //Generates a tone for when the player activates the trap and plays it.
-
+    /// <summary>
+    /// When a player picks up the red cube, plays a tone with these values to signify the trap being sprung. 
+    /// </summary>
     public void TrapSound()
     {
         Debug.Log("Trapped");
@@ -68,8 +84,10 @@ public class AudioTinker : MonoBehaviour {
         PlayOutAudio();
     }
 
-    //Generates a tone for when the trap obstacles hit the ground and plays it.
-
+    /// <summary>
+    /// When the trap has closed, plays a tone with these values to signify the blocks hitting the ground. 
+    /// The value for the frequency is randomised between two pre-set values to create a layered sound. 
+    /// </summary>
     public void ObstacleHitGroundSound()
     {
         ranFreq = Random.Range(350, 450);
@@ -79,6 +97,9 @@ public class AudioTinker : MonoBehaviour {
         PlayOutAudio();
     }
 
+    /// <summary>
+    /// When the player picks up the gold cube, plays a tone with these values to signify victory. 
+    /// </summary>
     public void Goal()
     {
         //timer between each PlayOutAudio??
